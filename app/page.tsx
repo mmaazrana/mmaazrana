be4260263b6467cd10d9e9b749637a99663bf5eb
Maaz Rana <maaz@MacBook-Pro.local>
2023-11-19 23:48:09 +0500

    added a few animations and header for next section

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import Head from "next/head";
 import Nav from "@/components/nav";
 import Hero from "@/components/hero";
+import Work from "@/components/work";
 
 export default function Home() {
   return (
     <main className="xl:max-w-8xl xl:mx-auto 2xl:p-12 2xl:gap-12 xl:p-11 xl:gap-11 lg:p-10 lg:gap-10 md:p-9 md:gap:9 sm:p-8 sm:gap-8 p-4 gap-4 flex flex-col items-center justify-between ">
       <Nav/>
         <Hero/>
+        <Work/>
     </main>
   )
 }
---
+++ components/work.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Work = () => {
+    return (
+        <div className={"w-full"}>
+            <h4 className={"font-body text-md xl:text-3xl lg:text-2xl md:text-xl sm:text-lg font-bold"} >My Work</h4>
+        </div>
+    );
+};
+
+export default Work;
