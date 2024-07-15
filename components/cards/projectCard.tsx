6cdc437a3050e658260229fdc0c07117145ebcb9
Muhammad Maaz <62180003+mmaazrana@users.noreply.github.com>
2024-07-16 04:20:28 +0500

    removed jittery effect from about us card

--- components/cards/projectCard.tsx
+++ components/cards/projectCard.tsx
@@ -26,7 +26,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
   return (
     <div
       id={"project"}
-      className={`group work hover:!-translate-y-4 overflow-hidden h-[320px] sm:h-[384px] md:h-[448px] lg:h-[512px] xl:h-[576px] items-start bg-secondary-hover rounded-3xl grid gap-8 sm:gap-9 md:gap-10 lg:gap-11 xl:gap-12 p-8 sm:p-9 md:p-10 lg:p-11 xl:p-12 ${alt ? "sm:basis-60 md:basis-2/5 lg:basis-1/3" : "flex-1 md:basis-3/5 lg:basis-2/3"} transition-all duration-300`}
+      className={`group work overflow-hidden h-[320px] sm:h-[384px] md:h-[448px] lg:h-[512px] xl:h-[576px] items-start bg-secondary-hover rounded-3xl grid gap-8 sm:gap-9 md:gap-10 lg:gap-11 xl:gap-12 p-8 sm:p-9 md:p-10 lg:p-11 xl:p-12 ${alt ? "sm:basis-60 md:basis-2/5 lg:basis-1/3" : "flex-1 md:basis-3/5 lg:basis-2/3"} transition-all duration-300`}
     >
       <div
         className={"flex flex-col gap-1 sm:gap-2 md:gap-2 lg:gap-3 xl:gap-3"}
