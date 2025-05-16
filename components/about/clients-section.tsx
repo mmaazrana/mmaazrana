19d2c2bf2d6f1da18cd51290aacafbe15ae4e5e0
Muhammad Maaz <62180003+mmaazrana@users.noreply.github.com>
2025-05-17 04:57:15 +0500

    fixed hydration issue

--- components/about/clients-section.tsx
+++ components/about/clients-section.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { FC, useRef, useState } from 'react'
+import React, { FC, useRef, useState, useEffect } from 'react'
 import AboutCard from '@/components/cards/about-card'
 import { clientIconComponents } from '@/helpers/constants'
 import SectionTitle from './section-title'
@@ -18,7 +18,13 @@ const ClientsSection: FC<ClientsSectionProps> = ({ className }) => {
   const [isHovered, setIsHovered] = useState(false)
   const isMobile = useMediaQuery({ maxWidth: 445 })
   const { theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
   const clientsRef = useRef(null)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   const clientsIsInView = useInView(clientsRef, { margin: `-45% 0px -45% 0px`, amount: 0.2 })
   const isActive = isHovered || (isMobile && clientsIsInView)
   const handleMouseEnter = () => {
@@ -45,17 +51,28 @@ const ClientsSection: FC<ClientsSectionProps> = ({ className }) => {
       >
         <div className='flex flex-col gap-2 sm:gap-3 md:gap-4 h-fit group'>
           <div className='flex flex-row flex-wrap gap-y-m gap-x-s relative group'>
-            {clientIconComponents.map(({ Icon }, index) => (
-              <div key={index} className='relative'>
-                <div
-                  className={`absolute inset-0 bg-primary-accent mix-blend-darken dark:mix-blend-color-dodge z-1 ${theme === 'light' && '!mix-blend-darken'} ${theme === 'dark' && '!mix-blend-color-dodge'}`}
-                />
-                <div
-                  className={`absolute inset-0 bg-secondary mix-blend-color z-1 transition-opacity duration-300 ${isActive ? 'opacity-0' : 'opacity-100'} group-hover:opacity-0`}
-                />
-                <Icon className='h-[24px] sm:h-[26px] md:h-[28px] lg:h-[30px] xl:h-[32px]' />
-              </div>
-            ))}
+            {clientIconComponents.map(({ Icon }, index) => {
+              let themeSpecificClasses = ''
+              if (mounted) {
+                if (theme === 'light') {
+                  themeSpecificClasses = '!mix-blend-darken'
+                } else if (theme === 'dark') {
+                  themeSpecificClasses = '!mix-blend-color-dodge'
+                }
+              }
+
+              return (
+                <div key={index} className='relative'>
+                  <div
+                    className={`absolute inset-0 bg-primary-accent mix-blend-darken dark:mix-blend-color-dodge z-1 ${themeSpecificClasses}`}
+                  />
+                  <div
+                    className={`absolute inset-0 bg-secondary mix-blend-color z-1 transition-opacity duration-300 ${isActive ? 'opacity-0' : 'opacity-100'} group-hover:opacity-0`}
+                  />
+                  <Icon className='h-[24px] sm:h-[26px] md:h-[28px] lg:h-[30px] xl:h-[32px]' />
+                </div>
+              )
+            })}
           </div>
         </div>
       </AboutCard>
